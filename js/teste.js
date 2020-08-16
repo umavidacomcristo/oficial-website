8890e2bff2f76f239a0a0d24deee93b9c7a4f5cc
matheusphalves <matheusphalves@gmail.com>
2020-08-16 20:23:43 -0300

    first commit files

---
+++ js/teste.js
@@ -0,0 +1,6 @@
+var about = document.querySelector("#about")
+
+about.addEventListener("click", function sobre(){
+    var caminho = document.getElementById("main")
+    var a = document.getElementById("main")
+})
