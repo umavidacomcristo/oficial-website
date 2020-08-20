cdedd14aa484c8fde2a5d538ab74ecdfc1a57cc7
matheusphalves <matheusphalves@gmail.com>
2020-08-19 21:06:32 -0300

    added new navbar and include BibbleAPI

--- js/teste.js
+++ js/teste.js
@@ -1,6 +1,22 @@
-var about = document.querySelector("#about")
+$(document).ready(function(){
+    
+jsonObj = loadNewVerse();
+console.log(jsonObj)
+insertVerse(jsonObj);
 
-about.addEventListener("click", function sobre(){
-    var caminho = document.getElementById("main")
-    var a = document.getElementById("main")
-})
+    
+});
+
+function loadNewVerse(){
+    let url = "https://bibleapi.co/api/verses/nvi/random"
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();
+    console.log(xhttp.responseText)
+    return JSON.parse(xhttp.responseText);
+}
+
+function insertVerse(jsonObject){
+    message = `${jsonObject.text}  ${jsonObject.book.name} ${jsonObject.chapter}:${jsonObject.number}`
+    $("#dayVerse").text(message);
+}
