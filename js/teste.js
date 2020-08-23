c28702e420f1250ba82e49eb8dc4aea073fb1d9e
matheusphalves <matheusphalves@gmail.com>
2020-08-22 22:13:45 -0300

    added error not found page and some itens in home page

--- js/teste.js
+++ js/teste.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     
 jsonObj = loadNewVerse();
-console.log(jsonObj)
+//console.log(jsonObj)
 insertVerse(jsonObj);
 
     
 });
 
-function loadNewVerse(){
+function loadNewVerse(){//método carrega versículo aleatório da API
     let url = "https://bibleapi.co/api/verses/nvi/random"
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, false);
@@ -16,7 +16,7 @@ function loadNewVerse(){
     return JSON.parse(xhttp.responseText);
 }
 
-function insertVerse(jsonObject){
+function insertVerse(jsonObject){//método insere versículo na tag com id 'dayVerse'
     message = `${jsonObject.text}  ${jsonObject.book.name} ${jsonObject.chapter}:${jsonObject.number}`
     $("#dayVerse").text(message);
 }
